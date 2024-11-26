82c0bf1700948bd8e9dd1cda759ff284def8a9c6
StianKoder <stian.t@jobloop.no>
2024-11-26 13:01:55 +0100

    oppdatering

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+const julekule = document.querySelector("#julekule")
+const paragraf = document.querySelector(".hidden")
+
+function click () {
+    const state = paragraf.style.display
+    if (state === "block") {
+        paragraf.style.display = "none"
+    } else {
+        paragraf.style.display = "block"
+    }
+}
+
+julekule.addEventListener("click", click)
